e40ba2e9ab6e5014128bc3daa53a9f97b0a5ff2a
tanjunjie <tan_junjie@126.com>
2023-01-13 17:37:30 +0800

    aaa111

---
+++ src/test/java/com/xf/A.java
@@ -0,0 +1,15 @@
+package com.xf;
+
+/**
+ * @author tanjunjie
+ * @Description TODO
+ * @Classname A
+ * @date 2023/1/13 17:36
+ * @time 17:36
+ */
+public class A {
+    public static void main(String[] args){
+      //
+        System.out.println(111);
+    }
+}
